420e21d7438053e30234166ead6b9216fa37793e
Adriano Navarro <Adriano_Navarro@outlook.com>
2022-03-27 16:31:36 -0300

    ADD: Base HTML.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav></nav>
+      <div className="video-container"></div>
+      <div className="poll"></div>
+      <div className="news">
+        <div className="news__article"></div>
+      </div>
+      <footer></footer>
     </div>
   );
 }
