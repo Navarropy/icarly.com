1ad2a1a6440c1626843bf99381a8176d336855fc
Adriano Navarro <Adriano_Navarro@outlook.com>
2022-03-28 22:48:44 -0300

    ADDED: Website Logo.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,21 @@
 import "./style/nav.scss"
+import { useEffect, useState } from 'react'
 
 export default function Nav(props) {
+
+    const [activeItem, setActiveitem] = useState(false)
+    console.log(activeItem)
+
+    function ActiveItem() {
+        console.log("aa")
+        setActiveitem(!activeItem)
+    }
+
     return(
         <nav>
+            <div className="icarly-logo">
+                <img src="./img/icarly-logo.png" width="auto" height="auto" />
+            </div>
             <div className="picture-container">
                 <img src="./img/characters-logo.png" alt="missing" width="auto" height="auto"/>
                 <div className="picture-container__background-box">
@@ -12,6 +25,7 @@ export default function Nav(props) {
             <div className="menu">
                 <div className="menu__lower-container">
                     <div className="menu__lower-container__menu-items">
+                        {/* <div onClick={ActiveItem} className={"menu__lower-container__menu-items__menu-item" + activeItem ? "" : " --menu-item-active"}></div> */}
                         <div className="menu__lower-container__menu-items__menu-item"></div>
                         <div className="menu__lower-container__menu-items__menu-item"></div>
                         <div className="menu__lower-container__menu-items__menu-item"></div>
